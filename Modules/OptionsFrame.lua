fc7c282f2acea37aa718e0e56db5cce8fd7000bc
B <159943278+Fr4ncoShade@users.noreply.github.com>
2025-12-25 20:56:03 +0300

    Add files via upload

--- Modules/OptionsFrame.lua
+++ Modules/OptionsFrame.lua
@@ -8,8 +8,8 @@ local lastShownTab
 local optionsFrame = Cell.CreateFrame("CellOptionsFrame", Cell.frames.mainFrame, 432, 401)
 Cell.frames.optionsFrame = optionsFrame
 PixelUtil.SetPoint(optionsFrame, "CENTER", CellParent, "CENTER", 1, -1)
-optionsFrame:SetFrameStrata("DIALOG")
-optionsFrame:SetFrameLevel(520)
+--optionsFrame:SetFrameStrata("DIALOG")
+--optionsFrame:SetFrameLevel(520)
 optionsFrame:SetClampedToScreen(true)
 optionsFrame:SetClampRectInsets(0, 0, 40, 0)
 optionsFrame:SetMovable(true)
